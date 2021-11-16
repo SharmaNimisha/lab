97abd04a9d5bf99fc14ad2fbd648259fda0cc970
SharmaNimisha <92383090+SharmaNimisha@users.noreply.github.com>
2021-11-16 19:37:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,4 @@
-provider "google" {
-        project = var.project
-        region = var.region
-}
-
-variable "machine" {
-	type = string
-	default = "f1-micro"
-}
-
-variable "project" {
-	type = string
-}
-
-variable "zone" {
-	type = string
-}
-
-variable "region" {
-	type = string
-}
-
-count = 3
-resource "google_compute_instance" "myvm.${count.index}" {
-        name = "dbsre.{count.index}"
-        machine_type = var.machine
-        zone = var.zone
-        network_interface {
-                network = "default"
-        }
-        boot_disk {
-                initialize_params {
-                        image = "debian-cloud/debian-10"
-        }
-        }
+resource "aws_iam_user" "example" {
+  count = 3
+  name  = "neo.${count.index}"
 }
