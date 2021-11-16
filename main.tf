f45e9f49c7cef71e3a771574a07ba4e3e7741c90
SharmaNimisha <92383090+SharmaNimisha@users.noreply.github.com>
2021-11-16 19:41:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ provider "google" {
         region = "us-central1"
 }
 
+variable "project" {
+        type = string
+}
+
 resource "aws_iam_user" "example" {
   count = 3
   name  = "neo.${count.index}"
